444ddea6c133a76dfe1b6d5c7fd08f88b51fb374
sussa3007 <sussa1933@gmail.com>
2023-09-19 23:02:43 +0900

    SampleTest 생성

---
+++ src/test/java/com/studyproject/cgopasa/bean/SampleTest.java
@@ -0,0 +1,16 @@
+package com.studyproject.cgopasa.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Test
+    void test() {
+        //  Bean List 출력해보기
+
+    }
+
+
+}
